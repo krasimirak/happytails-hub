dfb68740d03076811958975edb016dbd1f88da63
Krasimira Konstantinova <k_konstantinowa@hotmail.com>
2023-12-05 21:48:18 +0200

    Add account info

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,14 @@
+import { useAuth } from '../context/authContext';
+
 export default function Account() {
+    const { user } = useAuth();
+
     return (
         <main className="container px-4 mx-auto">
             <h1 className="text-4xl font-extrabold my-8">My Account</h1>
+
+            {user.displayName && <div>Name: {user.displayName}</div>}
+            <div>Email: {user.email}</div>
         </main>
     )
 }
